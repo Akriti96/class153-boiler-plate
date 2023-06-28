0f6bb77debb1b174bcd55adca40d733c1e2aefd1
Akriti96 <78339246+Akriti96@users.noreply.github.com>
2023-06-28 18:54:41 +0530

    Add files via upload

---
+++ js/Rotation.js
@@ -0,0 +1,13 @@
+//Diver rotation component
+AFRAME.registerComponent("diver-rotation-reader", {
+  schema: {
+    speedOfRoation: { type: "number", default: 0 },
+    speedOfAscent: { type: "number", default: 0 }
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+
+      
+    });
+  }
+});
